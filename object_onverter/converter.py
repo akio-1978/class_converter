73577c443b01b1e72b1e523fa13791f618c22bf1
ogawa.akio <akio.ogawa01@gmail.com>
2019-12-01 19:56:26 +0900

    オーバーライドするポイントをとして_convert_dictを作成

--- object_onverter/converter.py
+++ object_onverter/converter.py
@@ -1,6 +1,5 @@
 MAPPING_ROOT = '<root>'
 
-
 class ObjectConverter:
     # 生成時にマッピング定義を受け取る
     def __init__(self, *, mapping):
@@ -9,59 +8,66 @@ class ObjectConverter:
     # 変換の呼出しメソッド
     def convert(self, src):
         # 最上位の要素はマッピング'<root>'と必ずマッチする前提
-        return self._convert_value(self.mapping[MAPPING_ROOT], src)
+        return self._convert_value(MAPPING_ROOT, self.mapping[MAPPING_ROOT], src)
 
     # 値に従って処理方法を決める
-    def _convert_value(self, func, value):
+    def _convert_value(self, key, func, value):
         # リストの場合、要素全てをfuncで変換していく
         if isinstance(value, (list, tuple)):
-            return self._convert_sequence(func, value)
+            return self._convert_sequence(key, func, value)
 
         # dictの場合、そのままキーと値を取り出す
         if isinstance(value, dict):
-            return self._scan_dict(func, value)
+            return self._convert_dict(key, func, value)
 
         # classの場合__dict__を取り出してdictとして扱う
         if isinstance(value, object) and hasattr(value, '__dict__'):
-            return self._scan_dict(func, value.__dict__)
+            return self._convert_dict(key, func, value.__dict__)
 
         # どれにも該当しないものはそのまま返す
         return value
 
     # dictの中身を変換していく
-    def _scan_dict(self, func, src):
+    def _convert_dict(self, key, func, src):
+        # _call_functionで生成したオブジェクトに中身を詰める
+        return self._assign_dict(self._call_function(key, func, src), key, src)
 
-        created = func()
+    # mappingで指定されたオブジェクトの生成
+    def _call_function(self, key, func, src):
+        return func()
 
-        for key, value in src.items():
+    # dictの中身を取り出して当てはめて行く
+    def _assign_dict(self, dest, key, src):
+
+        for srcKey, value in src.items():
 
             # keyがマッピングに定義されている
-            if key in self.mapping:
-                func = self.mapping[key]
+            if srcKey in self.mapping:
+                func = self.mapping[srcKey]
                 # マッピングされた関数を実行して結果をセットする
-                self._set_value(created, key, self._convert_value(func, value))
+                self._set_value(dest, srcKey, self._convert_value(srcKey, func, value))
 
             # keyがマッピング定義にない物は、そのままセットする
             else:
                 # ここで渡されたvalueの中にマッピング定義された値があっても無視する
-                self._set_value(created, key, value)
+                self._set_value(dest, srcKey, value)
 
         # createdにsrcの中身が反映された状態
-        return created
+        return dest
 
     # リストの処理
-    def _convert_sequence(self, func, sequence):
+    def _convert_sequence(self, key, func, sequence):
         current = []
         for value in sequence:
-            current.append(self._convert_value(func, value))
+            current.append(self._convert_value(key, func, value))
         return current
 
     # dictとclass両方に対応する値setter
-    def _set_value(self, obj, key, value):
-        if isinstance(obj, dict):
-            obj[key] = value
+    def _set_value(self, dest, key, value):
+        if isinstance(dest, dict):
+            dest[key] = value
         else:
-            setattr(obj, key, value)
+            setattr(dest, key, value)
 
     # dict変換用インスタンスを得るユーティリティメソッド
     # もしOrderedDictを使いたい場合はdict_objectに指定する
